3a6c6c4e63a3190892acf79d7eda30bcab2cb304
himavanthv <113790263+himavanthv@users.noreply.github.com>
2026-08-31 23:46:21 +0530

    Update prepare_data.py

--- prepare_data.py
+++ prepare_data.py
@@ -3,8 +3,7 @@ import numpy as np
 import pandas as pd
 from datetime import datetime , timedelta, date
 import yfinance as yf
-from datetime import datetime, date, timedelta
-from Sendnotification import send_telegram_notification
+from datetime import datetime, date, timedelta
 from ModelBuilder import calculate_rsi, calculate_vwap
 import time
 import pytz
